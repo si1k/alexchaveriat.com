ceda76867c1192ce49e00633997e6237578f86a2
si1k <19499950+si1k@users.noreply.github.com>
2022-05-29 11:06:52 -0400

    Adding personal site settings

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,25 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'fa9cfdb0597347e9ac4dd34200bc10d2',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: null,
+  rootNotionSpaceId: '7e5675fb-db63-495a-962f-33b313515351',
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Alex Chaveriat Next.js Notion Dev',
+  domain: 'dev.alexchaveriat.com',
+  author: 'Alex Chaveriat',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Alex Chaveriat Personal Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'alexchaveriat',
+  github: 'si1k',
+  // linkedin: 'alex-chaveriat',
+  // youtube: 'alexchaveriat',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
