75a1101fa1ea5b77d911b609c794010f856ba144
si1k <19499950+si1k@users.noreply.github.com>
2022-06-08 23:11:26 -0400

    Add custom logo to header

--- site.config.ts
+++ site.config.ts
@@ -22,6 +22,10 @@ export default siteConfig({
   // linkedin: 'alex-chaveriat',
   // youtube: 'alexchaveriat',
 
+  // Custom logo - this will replace the breadcrumbs
+  // customHeaderLogo: 'https://cdn.logo.com/hotlink-ok/logo-social.png',
+  // customHeaderLogoDark: 'https://alexchaveriat-com.vercel.app/logo-dark.png',
+
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
   defaultPageIcon: 'https://alexchaveriat-com.vercel.app/page-icon.png',
