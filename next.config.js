ddabbb2f04dffe4deb48a653170ad6abd5286d79
si1k <19499950+si1k@users.noreply.github.com>
2022-06-10 21:59:01 -0400

    Updating domain name

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ module.exports = withBundleAnalyzer({
       'pbs.twimg.com',
       'abs.twimg.com',
       's3.us-west-2.amazonaws.com',
-      'alexchaveriat-com.vercel.app'
+      'alexchaveriat.com'
     ],
     formats: ['image/avif', 'image/webp'],
     dangerouslyAllowSVG: true,
